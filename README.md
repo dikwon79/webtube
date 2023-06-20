5f5ea7547a938a410cf65f615ffef18d130c98af
Dongil Kwon <dikwon79@naver.com>
2023-06-19 22:50:00 -0700

    database 전까지

---
+++ README.md
@@ -0,0 +1,14 @@
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+
+/users/edit -> Edit user
+/users/delete -> Delete My Profile
+/users/logout -> Log Out
+/videos/:id -> See video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload ->upload video
--- src/server.js
+++ src/server.js
@@ -1,41 +1,34 @@
 //const express = require("express");
+import "./db";
+import "./models/Video"
 import express from "express";
+import morgan from "morgan";
+import global from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+const PORT = 4000;
 
 const app = express();
-const PORT = 4000;
-const urlLogger = (req, res, next) => {
-    console.log(req.url);
-    next();
-};
-const timeLogger = (req, res, next) => {
-    const currentTime = Date.now();
-    const currentDate = new Date(currentTime);
-    const formattedDate = `${currentDate.getFullYear()}.${
-    currentDate.getMonth() + 1
-  }.${currentDate.getDate()}`;
 
-    console.log(`Time: ${formattedDate}`);
-    next();
-};
-const secuLogger = (req, res, next) => {
-    const protocol = req.protocol === "https" ? "secure" : "Insecure";
-    console.log(protocol);
-    next();
-};
-const protecLogger = (req, res, next) => {
-    // /protected로 이동하려는 경우 이동하지 못하도록 처리
-    if (req.url === "/protected") {
-        res.status(403).send("Access forbidden");
-    } else {
-        next();
-    }
-};
-app.use(urlLogger, timeLogger, secuLogger, protecLogger);
-app.get("/", (req, res) => res.send("<h1>Home</h1>"));
-app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
 
-// Codesandbox gives us a PORT :)
-app.listen(process.env.PORT, () => `Listening!✅`);
+// const logger = (req, res, next) => {
+//     console.log(`${req.method} ${req.url}`);
+//     next();
+// }
+
+const logger = morgan("dev");
+app.use(logger);
+
+app.set("view engine", "pug")
+app.set("views", process.cwd() + "/src/views");
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use("/", global);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
