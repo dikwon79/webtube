9e420d7bf4ac52f93d38119597b9a25c0b208bb7
dongil <dikwon79@naver.com>
2023-09-24 13:20:02 -0700

    request

---
+++ src/server_1.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`Server listineing on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
+
+
+
