79c3ef24445a4b2f5aa2c3b665f9cd36568b227c
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-07-10 13:58:22 -0700

    upload practice

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,7 +2,9 @@ import express from "express";
 import { 
     watch, 
     getEdit,
-    postEdit
+    postEdit,
+    getUpload,
+    postUpload
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -10,8 +12,6 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-
-//videoRouter.get("/:id(\\d+)/edit", getEdit);
-//videoRouter.post("/:id(\\d+)/edit", postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
