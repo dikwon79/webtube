dbf98ad3b436104fe24a824fd67b1144a0e50b8b
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-07-09 23:32:01 -0700

    mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,34 @@
-export const trending = (req, res) => 
-    res.render("home", {pageTitle : "Home", potato: "tomato"});
+
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "5 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 4,
+        comments: 3,
+        createdAt: "1 minutes ago",
+        views: 39,
+        id: 2,
+    },
+    {
+        title: "Third Video",
+        rating: 3,
+        comments: 2,
+        createdAt: "1 minutes ago",
+        views: 19,
+        id: 3,
+    },
+];
+export const trending = (req, res) => {
+    
+    return res.render("home", {pageTitle : "Home", videos});
+};
 export const see = (req, res) => res.render("watch");
 
 export const edit = (req, res) => res.render("edit");
