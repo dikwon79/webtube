b3edc0dfccb53fece693e0dde81dc1dec4832cc6
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-07-10 13:03:41 -0700

    edit video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,7 @@ let videos = [
         rating: 5,
         comments: 2,
         createdAt: "5 minutes ago",
-        views: 59,
+        views: 1,
         id: 1,
     },
     {
@@ -29,20 +29,26 @@ export const trending = (req, res) => {
     
     return res.render("home", {pageTitle : "Home", videos});
 };
-export const see = (req, res) => {
+export const watch = (req, res) => {
     const {id} = req.params;
     //const id = req.params.id;   둘다 같은 표현 위는 ES6버젼
     console.log("Show video", id);
     const video = videos[id - 1];
 
-    return res.render("watch", {pageTitle :`Watching ${video.title}`});
-};
-export const edit = (req, res) => res.render("edit");
-
-
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
+    return res.render("watch", {pageTitle :`Watching: ${video.title}`, video});
 };
 
+export const getEdit = (req, res) => {
+    const {id} = req.params;
+    const video = videos[id - 1];
+
+    return res.render("edit", { pageTitle: `Editing: ${video.title}`, video});
+
+};
+
+export const postEdit = (req, res) =>{
+    const {id} = req.params;
+    const { title } = req.body;
+    videos[id - 1].title = title;
+    return res.redirect(`/videos/${id}`);
+};
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {join} from "../controllers/userController";
+import {join, login} from "../controllers/userController";
 import {trending} from "../controllers/videoController";
 
 const globalRouter = express.Router();
@@ -7,5 +7,6 @@ const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
 
 export default globalRouter;
