5c0f2f7694c8b0e4c4ed5a8f5ef8c5bcfc1d0d1b
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-07-16 22:11:57 -0700

    edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,72 +1,64 @@
+import Video from "../models/Video";
 
-let videos = [
-    {
-        title: "First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "5 minutes ago",
-        views: 1,
-        id: 1,
-    },
-    {
-        title: "Second Video",
-        rating: 4,
-        comments: 3,
-        createdAt: "1 minutes ago",
-        views: 39,
-        id: 2,
-    },
-    {
-        title: "Third Video",
-        rating: 3,
-        comments: 2,
-        createdAt: "1 minutes ago",
-        views: 19,
-        id: 3,
-    },
-];
-export const trending = (req, res) => {
-    
+
+export const home = async(req, res) => {
+  
+    const videos = await Video.find({}); 
     return res.render("home", {pageTitle : "Home", videos});
+   
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const {id} = req.params;
-    //const id = req.params.id;   둘다 같은 표현 위는 ES6버젼
-    console.log("Show video", id);
-    const video = videos[id - 1];
-
-    return res.render("watch", {pageTitle :`Watching: ${video.title}`, video});
+   
+    const video = await Video.findById(id);
+    
+    if(!video){
+        return res.render("404", { pageTitle: "Video not found." });
+    }
+    return res.render("watch", { pageTitle : video.title, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async(req, res) => {
     const {id} = req.params;
-    const video = videos[id - 1];
-
-    return res.render("edit", { pageTitle: `Editing: ${video.title}`, video});
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", { pageTitle: "Video not found" });
+    }
+    return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 
 };
 
 export const postEdit = (req, res) => {
     const {id} = req.params;
     const { title } = req.body;
-    videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`);
 };
 
 export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle: "Upload Video"});
 };
-export const postUpload = (req, res) => {
-    const {title} = req.body;
-    const newVideo = {
-        title,
-        rating: 0,
-        comments: 0,
-        createdAt: "just now",
-        views: 0,
-        id: videos.length + 1,
+export const postUpload = async (req, res) => {
+    const {title, description, hashtags} = req.body;
 
-    };
-    videos.push(newVideo);
-    return res.redirect("/");
+    try{
+        await Video.create({
+            title: title,
+            description: description,
+            //createdAt: Date.now(),
+            hashtags: hashtags.split(",").map((word) => `#${word}`),
+            // meta: {
+            //     views: 0,
+            //     rating: 0,
+            // },
+    
+        });
+        return res.redirect("/");
+    } catch(error){
+        console.log(error);
+        return res.render("upload", {
+            pageTitle: "Upload Video", 
+            errorMessage: error._message,
+        });
+    }
+    
 };
---
+++ src/init.js
@@ -0,0 +1,13 @@
+import "./db";
+import "./models/Video";
+import app from "./index";
+
+
+const PORT = 4000;
+
+
+
+const handleListening = () =>
+    console.log(`server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
