8a17c2d75f7e76c7af81bbc4d0050856519cbc80
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-07-18 23:18:06 -0700

    search

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const home = async(req, res) => {
   
-    const videos = await Video.find({}); 
+    const videos = await Video.find({}).sort({createdAt: "desc"}); 
     return res.render("home", {pageTitle : "Home", videos});
    
 };
@@ -42,16 +42,11 @@ export const postEdit = async(req, res) => {
     }
 
     await Video.findByIdAndUpdate(id, {
-        title, description, hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+        title, 
+        description, 
+        hashtags: Video.formatHashtags(hashtags),
     });
-    // video.title = title;
-    // video.description = description;
-    // video.hashtags = hashtags
-    //     .split(",")
-    //     .map((word) => (word.startsWith("#") ? word : `#${word}`));
-    // await video.save();
+   
 
     return res.redirect(`/videos/${id}`);
 };
@@ -66,7 +61,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title: title,
             description: description,
-            hashtags,
+            hashtags: Video.formatHashtags(hashtags),
     
         });
         return res.redirect("/");
@@ -79,3 +74,25 @@ export const postUpload = async (req, res) => {
     }
     
 };
+export const deleteVideo = async (req, res) => {
+
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+
+    return res.redirect("/");
+}
+
+export const search = async (req, res) => {
+    const { keyword } = req.query;
+    let videos = [];
+
+    if(keyword){
+        videos = await Video.find({
+            title: keyword,
+
+        });
+        console.log(videos);
+    }
+    
+    return res.render("search", {pageTitle: "Search", videos});
+}
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,7 +4,8 @@ import {
     getEdit,
     postEdit,
     getUpload,
-    postUpload
+    postUpload,
+    deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,6 +13,7 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
